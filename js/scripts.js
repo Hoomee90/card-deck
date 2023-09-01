98539c97e05744f78362617d2577dc4b2c645023
Samantha Callie <Hoomee90@gmail.com>
2023-09-01 15:02:35 -0700

    add function to print card suits and values

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,29 @@
-//UI logic
-
+//Business Logic
 function constructCardValues() {
   let cardValues = Array.from({ length: 9 }, (value, position) => position);
   cardValues.push("Jack", "Queen", "King", "Ace");
   return cardValues
+} 
+
+//UI logic
+function printCard(value, suit) {
+  const h1 = document.querySelector("#title");
+
+  cardElement = document.createElement("p");
+  cardElement.append(value + " of " + suit);
+  h1.after(cardElement);
 }
 
 window.addEventListener("load", function() {
   const cardSuits = ["Clubs", "Diamonds", "Hearts", "Spades"];
-  const h1 = document.querySelector("#title");
   
   cardSuits.forEach(suit => {
     cardValues = constructCardValues();
-    cardValues.forEach(card => {
-      if (card) {
-
-      } else {
-
-      }
+    cardValues.forEach(value => {
+      if (Number.isInteger(value)){
+        value += 2
+      }     
+      printCard(value, suit);
     });
   });
 });
