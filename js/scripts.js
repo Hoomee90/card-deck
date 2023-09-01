a7e5b6ef0093ead026bd8f801426a84044ccffae
Samantha Callie <Hoomee90@gmail.com>
2023-09-01 16:01:24 -0700

    fix missing semicolon and variable declorations

--- js/scripts.js
+++ js/scripts.js
@@ -9,14 +9,14 @@ function constructCardValues() {
 function printCard(value, suit, index) {
   const h1 = document.querySelector("#title");
 
-  cardElement = document.createElement("p");
+  const cardElement = document.createElement("p");
   cardElement.append(value + " of " + suit);
   if (suit === "Diamonds" || suit === "Hearts") {
     cardElement.style.color = "red";
   } 
   h1.after(cardElement);
   if (index === 0) {
-    const br = document.createElement("br")
+    const br = document.createElement("br");
     document.querySelector("p").after(br);
   }
 }
@@ -25,10 +25,10 @@ window.addEventListener("load", function() {
   const cardSuits = ["Clubs", "Diamonds", "Hearts", "Spades"];
   
   cardSuits.forEach(suit => {
-    cardValues = constructCardValues();
+    const cardValues = constructCardValues();
     cardValues.forEach((value, index) => {
       if (Number.isInteger(value)){
-        value += 2
+        value += 2;
       }     
       printCard(value, suit, index);
     });
